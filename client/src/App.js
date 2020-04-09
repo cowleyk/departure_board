18d869de6cef4597b8289da68605700fe1db003b
Kevin Cowley <kevincowley@Kevins-MacBook-Pro-16144.local>
2020-04-09 00:11:32 -0600

    Use node server endpoint to fetch data

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ControlledExpansionPanels from './trains_table';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ControlledExpansionPanels />
+      </header>
+    </div>
+  );
+}
+
+export default App;
